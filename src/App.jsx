8eeb3ab932fe119b207b90eff30f61006a0f5721
Foparty <alfonsomartingarcia@gmail.com>
2023-04-17 21:23:49 +0200

    implemented suspense, lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 // import styles from './Index.module.css';
 import { Route, Routes } from 'react-router-dom';
-import Landing from './components/Landing/Landing';
-import About from './components/About/About';
-import Blog from './components/Blog/Blog';
-import Post from './components/Post/Post';
-import { request } from 'graphql-request';
-import NoMatch from './components/NoMatch/NoMatch';
+// import Landing from './components/Landing/Landing';
+// import About from './components/About/About';
+// import Blog from './components/Blog/Blog';
+// import Post from './components/Post/Post';
+// import NoMatch from './components/NoMatch/NoMatch';
+
+const Landing = lazy(() => import('./components/Landing/Landing'));
+const About = lazy(() => import('./components/About/About'));
+const Blog = lazy(() => import('./components/Blog/Blog'));
+const Post = lazy(() => import('./components/Post/Post'));
+const NoMatch = lazy(() => import('./components/NoMatch/NoMatch'));
 
 function App() {
-	const [posts, setPosts] = useState([]);
-
-	useEffect(() => {
-		const fetchPosts = async () => {
-			const { posts } = await request(
-				'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clgkwhsk53i2y01ujdim49nf8/master',
-				`
-				{
-					posts {
-						id
-						slug
-						title
-						postImages {
-							url
-						}
-						featuredImage {
-							url
-						}
-						categories {
-							id
-							name
-							slug
-						}
-						content {
-							raw
-						}
-						createdAt
-					}
-				}
-				`,
-			);
-
-			setPosts(posts);
-		};
-		fetchPosts();
-	}, []);
 	return (
 		<>
-			<Routes>
-				<Route path={'/'} element={<Landing />} />
-				<Route path={'/about'} element={<About />} />
-				<Route path={'/blog'} element={<Blog posts={posts} />} />
-				<Route path={'/post/:slug'} element={<Post posts={posts} />} />
-				<Route path='*' element={<NoMatch />} />
-			</Routes>
+			<Suspense fallback={<div className='loadingcontainer'>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<Landing />} />
+					<Route path={'/about'} element={<About />} />
+					<Route path={'/blog'} element={<Blog />} />
+					<Route path={'/post/:slug'} element={<Post />} />
+					<Route path='*' element={<NoMatch />} />
+				</Routes>
+			</Suspense>
 		</>
 	);
 }
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Blog.module.css';
 import { Link } from 'react-router-dom';
 import PostCard from '../PostCard/PostCard';
+import request from 'graphql-request';
 
-const Blog = ({ posts }) => {
+const Blog = () => {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const { posts } = await request(
+				'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clgkwhsk53i2y01ujdim49nf8/master',
+				`
+				{
+					posts {
+						id
+						slug
+						title
+						postImages {
+							url
+						}
+						featuredImage {
+							url
+						}
+						categories {
+							id
+							name
+							slug
+						}
+						content {
+							raw
+						}
+						createdAt
+					}
+				}
+				`,
+			);
+
+			setPosts(posts);
+		};
+		fetchPosts();
+	}, []);
 	console.log(posts);
 
 	return (
